142294c1b723376e4b8e941489e0f0e5fbfe1753
Your Name <you@example.com>
2020-10-19 12:03:25 +0600

    LabTaskdone

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.Remoting.Services;
+
+namespace LabTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello");
+            Console.WriteLine("Nura Jannat Rakhi");
+            int a = 50, b = 5, sum = 50 + 5;
+            Console.WriteLine("Sum is :" + sum);
+
+            int d = 50 / 5;
+            Console.WriteLine("Divition is :" + d);
+            int e = -5 + 8 * 6;
+            Console.WriteLine("a." + e);
+            int f = (55 + 9) % 9;
+            Console.WriteLine("b." + f);
+            int g = 20 + -3 * 5 / 8;
+            Console.WriteLine("c." + g);
+            int h = 5 + 15 / 3 * 2 - 8 % 3;
+            Console.WriteLine("d." + h);
+            int s = 4 + 5;
+            Console.WriteLine("Sum=" + s);
+            int multy = 4 * 5;
+            Console.WriteLine("Multiply=" + multy);
+            int sub = 5 - 4;
+            Console.WriteLine("Substract=" + sub);
+            int div = 40 / 4;
+            Console.WriteLine("Divide=" + div);
+            int rem = 30 / 7;
+            Console.WriteLine("Reminder=" + rem);
+            Double exp = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            Console.WriteLine("Exp :" + exp);
+            double width = 5.5, height = 8.5;
+            Console.WriteLine("Area is" + width * height);
+            Console.WriteLine("Perameter is" + 2 * (width + height));
+
+        
+
+
+
+
+
+
+        }
+    }
+}
